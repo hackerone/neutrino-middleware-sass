dfb90483d74d3ed2423a39441df08c3264053cc8
Martin Liberg <martin.liberg@izettle.com>
2017-04-26 20:43:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require("path")
+
+const CSS_LOADER = require.resolve("css-loader")
+const SASS_LOADER = require.resolve("sass-loader")
+const STYLE_LOADER = require.resolve("style-loader")
+
+module.exports = neutrino => {
+  neutrino.config.module
+    .rule("scss")
+    .test(/\.scss$/)
+    .use("style")
+      .loader(STYLE_LOADER)
+      .end()
+    .use("css")
+      .loader(CSS_LOADER)
+      .end()
+    .use("sass")
+      .loader(SASS_LOADER)
+      .options({
+        includePaths: [neutrino.options.source]
+      })
+}
